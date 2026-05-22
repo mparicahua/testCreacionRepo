68df2632611df0e3e65deed422ef38730caab803
Lov-Oro <paolo.lovon@ucsp.edu.pe>
2026-05-22 14:39:20 +0000

    feat: insertar usuarios

---
+++ Tarea02 - scripts Insert/usuarios.sql
@@ -0,0 +1,6 @@
+INSERT INTO usuarios ( email, password_hash, activo) VALUES
+    ('xdxdxdxdx@hotmail.com', 'contraseña'),
+    ('xxxxxxxxx@gmail.com' , 'PerroRabioso'),
+    ('YYYYYYYYY@icloud.com' , 'AYCHICHI'),
+    ('zzzzzzzz@gmail.com', 'NOOOOOO' , FALSE),
+    ('HOLAXD@hotmail.com', 'SIIIIII');
