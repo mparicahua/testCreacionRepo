2095996f5177e11dd37715da94abab21a3f086b3
Mharco Paricahua Akutak <132587489+mparicahua@users.noreply.github.com>
2026-05-22 09:47:59 -0500

    feat:scrip poblar tabla personas

---
+++ Tarea02 - scripts Insert/personas.sql
@@ -0,0 +1,6 @@
+INSERT INTO personas (nombres, apellidos, dni, fecha_nacimiento, genero, telefono, email_contacto, ubigeo_id, direccion) VALUES
+( 'Carlos Alberto', 'Gomez Mendoza', '45871234', '1990-05-15', 'Masculino', '987654321', 'carlos.gomez@email.com', 1, 'Av. Arequipa 1420, Dpto 402'),
+( 'Ana Lucia', 'Rodriguez Quispe', '70214589', '1995-09-22', 'Femenino', '951753456', 'ana.rodriguez@email.com', 2, 'Calle Mercaderes 312'),
+( 'Juan Carlos', 'Benites Flores', '10457812', '1983-12-02', 'Masculino', '963258741', 'juan.benites@email.com', 2, 'Jr. Pizarro 545, Urb. El Recreo'),
+( 'Maria Elena', 'Chavez Condori', '42158963', '1988-07-08', 'Femenino', '942158763', 'maria.chavez@email.com', 3, 'Av. El Sol 845'),
+( 'Luis Fernando', 'Palacios Vega', '48563214', '2000-01-30', 'Masculino', '914725836', 'luis.palacios@email.com', 1, 'Urb. Miraflores Mz. F Lote 12');
