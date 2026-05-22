9a28e5d3ab10798c60cef279579935b95a3e528e
Arturo Josue Benavente Vilca <arturo.benavente@ucsp.edu.pe>
2026-05-22 09:39:55 -0500

    add: Insertar datos en tabla categorias_delito

---
+++ Tarea02 - scripts Insert/categorias_delito_insert.sql
@@ -0,0 +1,7 @@
+INSERT INTO "categorias_delito" (nombre, descripcion, activo)
+VALUES 
+    ("Delito contra el patrimonio", "Sustrajo pertenencias utilizando violencia e intimidación", true),
+    ("Delito contra la fe pública", "Engañó a varias personas para obtener dinero de forma fraudulenta", true),
+    ("Delito contra la salud pública", "Transportaba y distribuía sustancias ilegales.", false),
+    ("Delito contra la administración pública", "Utilizó su cargo público para recibir beneficios indebidos", false),
+    ("Delito contra la vida, el cuerpo y la salud", "Realizó agresiones físicas y psicológicas dentro del entorno familiar", true);
