a4c2cb92d902ad0cd59c19cc18d50f089c617fbb
Lov-Oro <paolo.lovon@ucsp.edu.pe>
2026-06-03 05:22:51 +0000

    fix: corrige y simplica codigo C08

--- Entregable-01 Consultas /C08.sql
+++ Entregable-01 Consultas /C08.sql
@@ -1,26 +1,12 @@
--- Denunciantes
 SELECT 
-    'Denunciante' AS "Tipo de implicado", 
-    P.numero_documento AS "Documento", 
-    P.nombres ||' '|| P.apellidos AS "Nombre"
-FROM denuncias D 
-INNER JOIN "personas" P ON D.denunciante_id = P.id
-WHERE D.id = :id_denuncia
-
-UNION ALL
-
-SELECT 
-    'Denunciado', 
-    DN.dni, 
-    DN.nombres ||' '|| DN.apellidos
-FROM denunciados DN
-WHERE DN.denuncia_id = :id_denuncia
-
-UNION ALL
-
-SELECT 
-    'Testigo', 
-    T.dni, 
-    T.nombres ||' '|| T.apellidos
-FROM testigos T
-WHERE T.denuncia_id= :id_denuncia;
+    (p.nombres || ' ' || p.apellidos) AS nombre_del_fiscal,
+    f.especialidad,
+    df.nombre AS distrito_fiscal,
+    d.fecha_derivacion,
+    d.estado,
+    d.motivo
+FROM derivaciones d
+INNER JOIN fiscales f ON d.fiscal_id = f.id
+INNER JOIN personas p ON f.persona_id = p.id
+INNER JOIN distritos_fiscales df ON f.distrito_fiscal_id = df.id
+WHERE d.denuncia_id = 3;
