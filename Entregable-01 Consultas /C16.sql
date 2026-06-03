fa0c34f30ec47798651b14752b66039da6a514ae
Lov-Oro <paolo.lovon@ucsp.edu.pe>
2026-06-03 16:22:41 +0000

    fix: corrige aisgnacion de consulta C16

--- Entregable-01 Consultas /C16.sql
+++ Entregable-01 Consultas /C16.sql
@@ -1,11 +1,14 @@
 SELECT 
-    D.categoria AS "Gravedad",
-    COUNT(D.id_delito) AS "Cantidad de delitos"
-
-FROM Denuncia_Delito DD
-INNER JOIN Delitos D ON DD.id_delito = D.id_delito
-
-WHERE DD.id_denuncia = :id_denuncia
-
-GROUP BY D.categoria
-HAVING COUNT(D.id_delito) >=1;
+    TD.nombre AS nombre_del_tipo_delito,
+    CD.nombre AS categoria,
+    COUNT(D.id) AS total_denuncias,
+    ROUND((COUNT(D.id) * 100.0) / (SELECT COUNT(*) FROM denuncias), 2) AS porcentaje
+FROM denuncias D
+INNER JOIN tipos_delito TD ON D.tipo_delito_id = TD.id
+INNER JOIN categorias_delito CD ON TD.categoria_id = CD.id
+WHERE 1 = 1
+    -- AND D.fecha_hecho >= '2026-01-01 00:00:00'
+    -- AND D.fecha_hecho <= '2026-12-31 23:59:59'
+GROUP BY TD.id, TD.nombre, CD.nombre
+HAVING COUNT(D.id) > 0
+ORDER BY total_denuncias DESC;
