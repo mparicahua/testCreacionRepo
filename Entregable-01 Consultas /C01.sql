0d34489dad2accf3a4f06d3b0fc4f0b020552a8f
Arturo Josue Benavente Vilca <arturo.benavente@ucsp.edu.pe>
2026-06-03 23:18:28 -0500

    feat: Creación de consulta para saber el estado actual y detalle de una denuncia específica

---
+++ Entregable-01 Consultas /C01.sql
@@ -0,0 +1,19 @@
+SELECT
+    d.codigo_seguimiento,
+    d.descripcion_hecho,
+    d.fecha_hecho,
+    td.nombre AS tipo_delito,
+    cd.nombre AS categoria_delito,
+    ed.nombre AS estado_actual,
+    CASE
+        WHEN d.es_anonima = true THEN 'ANÓNIMO'
+        ELSE p.nombres || ' ' || p.apellidos
+    END AS nombre_denunciante,
+    df.nombre AS distrito_fiscal
+FROM denuncias d
+INNER JOIN tipos_delito td ON td.id = d.tipo_delito_id
+INNER JOIN categorias_delito cd ON cd.id = td.categoria_id
+INNER JOIN estados_denuncia ed ON ed.id = d.estado_id
+INNER JOIN personas p ON p.id = d.denunciante_id
+LEFT JOIN distritos_fiscales df ON df.id = d.distrito_fiscal_id
+WHERE d.codigo_seguimiento = 'DEN-2024-00001';
